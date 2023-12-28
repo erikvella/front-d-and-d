9eba2ac3feb48212e1ea879d3c3cecdcd7270dd0
silviapetrino <petrino.silvia@gmail.com>
2023-12-28 13:04:48 +0100

    [add] input search fuction

--- src/components/partials/Header.vue
+++ src/components/partials/Header.vue
@@ -1,12 +1,31 @@
   <script>
+  import axios from 'axios';
+  import { store } from '../../data/store'
     export default {
       name: 'Header',
       data() {
-        return {}
+        return {
+          tosearch:''
+        }
       },
       computed: {},
       mounted() {},
-      methods: {}
+      methods: {
+        getApi(){
+          const endpoint = (this.tosearch.length > 0) ? store.apiUrl + 'search/' + this.tosearch : store.apiUrl + 'characters/'
+          axios.get(endpoint)
+           .then(results => {
+            console.log(endpoint)
+            store.characters = results.data.data;
+            store.paginator.links = results.data.links;
+            store.paginator.firstPageUrl = results.data.first_page_url;
+            store.paginator.lastPageUrl = results.data.last_page_url;
+            store.paginator.currentPage = results.data.current_page;
+            store.paginator.lastPage = results.data.last_page;
+            this.tosearch = '';
+           })
+        }
+      }
     }
   </script>
 <template>
@@ -27,6 +46,14 @@
         </li>
       </ul>
     </nav>
+    <div class="form">
+      <input
+        v-model.trim = "tosearch"
+        placeholder = "Cerca"
+        type = "text"
+        @keyup.enter = "getApi"
+      >
+    </div>
   </header>
 </template>
 
