32174d70d0b521fe5a27e3dd3e4bb4c952db43d5
franedsandi <franedsandi@gmail.com>
2023-12-29 15:39:59 +0100

    [feat] message sender

--- src/router.js
+++ src/router.js
@@ -13,46 +13,46 @@ history: createWebHistory(),
 // customizzo la classe predefinita del router-link
 linkExactActiveClass : 'active',
 routes:[
-  {
-     path: '/',
-     name: 'home',
-     component: Home
-  },
-  {
-     path: '/chi-siamo',
-     name: 'about',
-     component: About
-  },
-  {
-     path: '/blog',
-     name: 'blog',
-     component: Blog
-  },
-  {
-     path: '/contatti',
-     name: 'contacts',
-     component: Contacts
-  },
-  {
-    // rotta con elemento dinamico come parametro (slug)
-     path: '/dettaglio-personaggio/:slug',
-     name: 'characterDetail',
-     component: CharacterDetail
-  },
-  {
-    // rotta con elemento dinamico come parametro (slug)
-     path: '/personaggi-razza/:race_slug',
-     name: 'charactersRace',
-     component: CharactersRace
-  },
+   {
+      path: '/',
+      name: 'home',
+      component: Home
+   },
+   {
+      path: '/chi-siamo',
+      name: 'about',
+      component: About
+   },
+   {
+      path: '/blog',
+      name: 'blog',
+      component: Blog
+   },
+   {
+      path: '/contatti',
+      name: 'contacts',
+      component: Contacts
+   },
+   {
+      // rotta con elemento dinamico come parametro (slug)
+      path: '/dettaglio-personaggio/:slug',
+      name: 'characterDetail',
+      component: CharacterDetail
+   },
+   {
+      // rotta con elemento dinamico come parametro (slug)
+      path: '/personaggi-razza/:race_slug',
+      name: 'charactersRace',
+      component: CharactersRace
+   },
 
-  // rotta 404 da mettere come ultima rotta
-  {
-    path: '/:pathMatch(.*)*',
-    // il nome possiamo anche non metterlo
-    name: 'error404',
-    component: Error404
- }
+   // rotta 404 da mettere come ultima rotta
+   {
+      path: '/:pathMatch(.*)*',
+      // il nome possiamo anche non metterlo
+      name: 'error404',
+      component: Error404
+   }
 ]
 });
 
