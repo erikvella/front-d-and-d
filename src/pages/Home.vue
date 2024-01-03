7407c32d381564a3cd9c713dba5e5e11696b4017
silviapetrino <petrino.silvia@gmail.com>
2024-01-03 11:45:45 +0100

    [edit] home page, about us

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -16,11 +16,13 @@
       <p class="lh-base">
         Welcome adventurers, guardians of realms, and wizards of magical lands ! <br />
         On this site, you can view the characters from your database. <br />
+      </p>
+      <p>
         Are you ready to immerse yourselves in an epic world where magic dances with the sword, and legends come to life? "Epic Realms: Unconquerable Legends" is the project that will lead you through timeless adventures in the heart of the Forgotten Realms. In this ambitious Dungeons & Dragons role-playing project, we invite you to explore unknown lands, face legendary monsters, and forge your destiny in a universe rich with mysteries. Whether you're a seasoned knight seeking fame, a cunning wizard in search of ancient secrets, or a stealthy rogue hungry for hidden treasures, "Epic Realms" offers a tailor-made gaming experience for every type of hero. 
       </p>
-      <a>
-        
-      </a>
+      <router-link  :to="{name: 'blog'}" class="btn btn-light">
+        Go to blog section
+      </router-link>
     </div>
 
     
